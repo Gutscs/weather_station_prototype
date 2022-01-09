9615c34a25caa39a2a7b7ba56aec1d7794ae11a4
Gutscs <gutscs@gmail.com>
2022-01-09 11:15:32 -0300

    :round_pushpin: Add file for pins and channels

---
+++ libraries/weather_station/pins_and_channels.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file pins_and_channels.hpp
+ *
+ * @brief pins and channels definitions
+ * 
+ * @copyright MIT License
+ */
+
+// HCSR04 sensor pinout
+#define HCTRIGGER PD4
+#define HCECHO PD5
+
+// DHT sensor pinout
+#define DHTPIN A1
