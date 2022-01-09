acd8eb2d2376c68770753b95bdc7b84da665747f
Gutscs <gutscs@gmail.com>
2022-01-09 11:06:20 -0300

    :dash: Moved file

---
+++ libraries/weather_station/config.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file  config.hpp
+ *
+ * @brief code configuration
+ * 
+ * @copyright MIT License
+ */
+
+// constants
+#define SERIAL_VEL 115200
+#define SAMPLING_TIME 300000 
+
+// DHT11 sensor config
+#define DHTTYPE DHT11 
+
+// BMP180 sensor config
+#define SEA_LEVEL_PRESSURE 101500
+
+// HCSR04 sensor config
+#define SENSOR_HEIGHT_IN_M 0.15
+
+// Time config
+#define TIME_HEADER  "T"
+#define TIME_REQUEST  7 
+#define DEFAULT_TIME 1357041600
+
+
